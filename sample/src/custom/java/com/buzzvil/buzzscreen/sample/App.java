7a2d63ee244d10a43ff1907f824c60efc0cfe559
bongchul kim <tiredtire@gmail.com>
2015-10-08 21:41:54 +0900

    update sdk

--- sample/src/custom/java/com/buzzvil/buzzscreen/sample/App.java
+++ sample/src/custom/java/com/buzzvil/buzzscreen/sample/App.java
@@ -15,8 +15,5 @@ public class App extends Application {
         // R.drawable.image_on_fail : 네트워크 에러 혹은 일시적으로 잠금화면에 보여줄 캠페인이 없을 경우 보여주게 되는 이미지.
         // useMultiProcess : 잠금화면 서비스를 분리된 프로세스에서 실행하는 경우 true, 사용하지 않으면 false
         BuzzScreen.init("app_key", this, CustomLockerActivity.class, R.drawable.image_on_fail, false);
-
-        // 정각 적립금 요청 트래픽을 분산하기 위한 팩터. 자세한 설명은 연동 가이드 참고.
-//        BuzzScreen.setBasePointsSpreadingFactor(10);
     }
 }
