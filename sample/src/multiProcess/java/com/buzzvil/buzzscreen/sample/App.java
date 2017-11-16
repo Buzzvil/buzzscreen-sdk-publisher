97ba8340f4b3eda729decbdda669349c5bff313f
patrick <bckim@buzzvil.com>
2017-11-16 13:41:38 +0900

    remove deprecated method

--- sample/src/multiProcess/java/com/buzzvil/buzzscreen/sample/App.java
+++ sample/src/multiProcess/java/com/buzzvil/buzzscreen/sample/App.java
@@ -15,6 +15,6 @@ public class App extends Application {
         // SimpleLockerActivity.class : 잠금화면 액티비티 클래스
         // R.drawable.image_on_fail : 네트워크 에러 혹은 일시적으로 잠금화면에 보여줄 캠페인이 없을 경우 보여주게 되는 이미지.
         // useMultiProcess : 잠금화면 서비스를 분리된 프로세스에서 실행하는 경우 true, 사용하지 않으면 false
-        BuzzScreen.init("app_key", this, SimpleLockerActivity.class, R.drawable.image_on_fail, true);
+        BuzzScreen.init("app_key", this, SimpleLockerActivity.class, R.drawable.image_on_fail);
     }
 }
