d480725b3cb91772ff73c463a507b77b449a5d19
ybbarng <ybbarng@gmail.com>
2018-05-23 18:52:24 +0900

    개인정보 이용 동의 테스트를 위한 UI 추가 및 코드는 주석으로 추가

--- sample/src/main/java/com/buzzvil/buzzscreen/sample/MainActivity.java
+++ sample/src/main/java/com/buzzvil/buzzscreen/sample/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import com.buzzvil.buzzscreen.sdk.BuzzScreen;
 import com.buzzvil.buzzscreen.sdk.UserProfile;
@@ -13,6 +15,7 @@ import java.util.Random;
 
 public class MainActivity extends Activity {
     final static String TAG = "MainActivity";
+    boolean privacyConsentEnabled = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +26,7 @@ public class MainActivity extends Activity {
         // 앱 실행시 처음 실행되는 액티비티에 추가해 준다.
         BuzzScreen.getInstance().launch();
 
-        Button btnLockerOn = (Button)findViewById(R.id.locker_on);
+        Button btnLockerOn = (Button) findViewById(R.id.locker_on);
         btnLockerOn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,16 +43,65 @@ public class MainActivity extends Activity {
                 userProfile.setRegion("서울특별시 관악구");
 
                 // 버즈스크린 활성화
-                BuzzScreen.getInstance().activate();
+                if (privacyConsentEnabled) {
+                    BuzzScreen.getInstance().activate();
+                } else {
+                    /*
+                    BuzzScreen.getInstance().activateIfConsent(MainActivity.this, new BuzzScreen.OnActivateListener() {
+                        @Override
+                        public void onActivated() {
+                            Toast.makeText(MainActivity.this, "Activated", Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onCancelledByUser() {
+                            Toast.makeText(MainActivity.this, "Cancelled", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    */
+                }
             }
         });
 
-        Button btnLockerOff = (Button)findViewById(R.id.locker_off);
+        Button btnLockerOff = (Button) findViewById(R.id.locker_off);
         btnLockerOff.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // 버즈스크린 비활성화
                 BuzzScreen.getInstance().deactivate();
+            }
+        });
+
+        final Button privacyConsentRevokeButton = findViewById(R.id.privacy_consent_revoke);
+        ((CheckBox) findViewById(R.id.checkbox_test_privacy_consent)).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                privacyConsentEnabled = isChecked;
+                privacyConsentRevokeButton.setVisibility(privacyConsentEnabled ? View.VISIBLE : View.GONE);
+            }
+        });
+
+        privacyConsentRevokeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*
+                BuzzScreen.getInstance().showRevokeConsentDialog(MainActivity.this, new BuzzScreen.OnConsentRevokeListener() {
+                    @Override
+                    public void onConsentRevoked() {
+                        Toast.makeText(MainActivity.this, "Revoked", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onDeactivated() {
+                        Toast.makeText(MainActivity.this, "Deactivated", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCancelledByUser() {
+                        Toast.makeText(MainActivity.this, "Cancelled", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                */
             }
         });
     }
