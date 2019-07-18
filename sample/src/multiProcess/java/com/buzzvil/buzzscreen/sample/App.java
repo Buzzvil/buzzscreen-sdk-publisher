e2079c208ecbb419fef84e2cf853b948173b1251
Jineon Kim <cos@getsliejoy.com>
2019-07-18 12:38:45 +0900

    STU background customizing

--- sample/src/multiProcess/java/com/buzzvil/buzzscreen/sample/App.java
+++ sample/src/multiProcess/java/com/buzzvil/buzzscreen/sample/App.java
@@ -3,6 +3,8 @@ package com.buzzvil.buzzscreen.sample;
 import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDex;
+import android.support.v4.content.ContextCompat;
+import android.widget.ImageView;
 
 import com.buzzvil.buzzscreen.sdk.BuzzScreen;
 import com.buzzvil.buzzscreen.sdk.SecurityConfiguration;
@@ -23,8 +25,15 @@ public class App extends Application {
         // SimpleLockerActivity.class : 잠금화면 액티비티 클래스
         // R.drawable.image_on_fail : 네트워크 에러 혹은 일시적으로 잠금화면에 보여줄 캠페인이 없을 경우 보여주게 되는 이미지.
         BuzzScreen.init("app_key", this, SimpleLockerActivity.class, R.drawable.image_on_fail, Constants.useGDPR ? BuzzScreen.PrivacyPolicy.GDPR : BuzzScreen.PrivacyPolicy.NONE);
-        BuzzScreen.getInstance().setSecurityConfiguration(new SecurityConfiguration.Builder().
-                withSecurity(Constants.useSecurity).
-                build());
+        BuzzScreen.getInstance().setSecurityConfiguration(
+                new SecurityConfiguration.Builder()
+                        .withSecurity(Constants.useSecurity)
+                        .backgroundResourceId(R.drawable.image_stu_center)
+                        .backgroundImageScaleType(ImageView.ScaleType.FIT_CENTER)
+                        .backgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.colorSecurityBackground))
+                        .backgroundDimAlpha(0f)
+                        .showClock(false)
+                        .showDescription(true)
+                        .build());
     }
 }
